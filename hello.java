8e359b17c215261397c27c65e4076546fdf672a6
AidanBattad <142377920+AidanBattad@users.noreply.github.com>
2023-08-16 18:05:37 +0800

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class Hello {
   public static void main(string[] args){
-    System.out.println(" Software Engineering Lab 1");
-     System.out.println(" Student Name: Aidan Battad");
+    System.out.println(" Hello, Software Engineering Course!");
+     System.out.println(" My name is Battad, Aidan Liam!");
   }
 }
