c0e81de0aeeb35575dbfcfdd18b880b9fc390847
Aidan Battad <aidanliambattad@gmail.com>
2023-09-04 23:14:26 +0800

    Assignment addition

--- hello.java
+++ hello.java
@@ -3,6 +3,7 @@ public class Hello {
     System.out.println("Hello, Software Engineering Course!");
      System.out.println("My name is Battad, Aidan Liam!");
 	System.out.println("That is why developers are highly-paid!");
+	 System.out.println ("Complicated world of SoftDev that is why it pays much!");
 
   }
 }
